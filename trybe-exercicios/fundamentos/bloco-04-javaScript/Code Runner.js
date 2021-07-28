bd7dca80a4ffb1b0ae22eef93eeef3104737cd36
claudinei <claudineigraunki@hotmail.com>
2021-07-28 19:45:55 -0300

    Resolucao exercicio 03 - modulo 4.1

---
+++ trybe-exercicios/fundamentos/bloco-04-javaScript/Code Runner.js
@@ -0,0 +1,21 @@
+//Exercício 01
+// let a = 10;
+// let b = 5;
+// console.log("soma:" + (a + b));
+// console.log("subtração:" + (a - b));
+// console.log("Multiplicação:" + (a * b));
+// console.log("Divisão:" + (a / b));
+// console.log("Módulo:" + (a % b));
+
+//Exercício 01
+
+// let n1 = 20;
+// let n2 = 40;
+// if(n1 > n2){
+//     console.log("n1 é maior que n2");
+// }
+// else{
+//     console.log("n2 é maior que n1");
+// };
+
+//Exercício 03
